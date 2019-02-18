a652195c10f63817f1a03f114e8db35f91cf60d0
uxun <zhongshixun0201@gmail.com>
2019-02-18 22:37:03 +0800

    fix download.sh

--- down/download.sh
+++ down/download.sh
@@ -1,46 +1,52 @@
 #!/bin/bash
 # This script describes where to download the official released binaries needed 
-# It's suggested to download the entire *.tar.gz at https://pan.baidu.com/s/1c4RFaA
 
 # example releases
-K8S_VER=v1.10.4
-ETCD_VER=v3.3.8
+K8S_VER=v1.13.3
+ETCD_VER=v3.2.24
 DOCKER_VER=17.03.2-ce
 CNI_VER=v0.6.0
 DOCKER_COMPOSE=1.18.0
-HARBOR=v1.5.2
+HARBOR=v1.7.0
 
-echo "\nNote1: Before this script, please finish downloading binaries manually from following urls."
-echo "\nNote2：If binaries are not ready, use `Ctrl + C` to stop this script."
+echo -e "\nNote1: Before this script, please finish downloading binaries manually from following urls."
+echo -e "\nNote2：If binaries are not ready, use "Ctrl + C" to stop this script."
 
-echo "\n----download k8s binary at:"
+echo -e "\n----download k8s binary at:"
 echo https://dl.k8s.io/${K8S_VER}/kubernetes-server-linux-amd64.tar.gz
 
-echo "\n----download etcd binary at:"
+echo -e "\n----download etcd binary at:"
 echo https://github.com/coreos/etcd/releases/download/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz
-echo https://storage.googleapis.com/etcd/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz
 
-echo "\n----download docker binary at:"
+echo -e "\n----download docker binary at:"
 echo https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VER}.tgz
 
-echo "\n----download ca tools at:"
+echo -e "\n----download ca tools at:"
 echo https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
 echo https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
 echo https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
 
-echo "\n----download docker-compose at:"
+echo -e "\n----download docker-compose at:"
 echo https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE}/docker-compose-Linux-x86_64
 
-echo "\n----download harbor-offline-installer at:"
-echo https://github.com/vmware/harbor/releases/download/${HARBOR}/harbor-offline-installer-${HARBOR}.tgz
+echo -e "\n----download harbor-offline-installer at:"
+echo https://storage.googleapis.com/harbor-releases/release-1.7.0/harbor-offline-installer-${HARBOR}.tgz
 
-echo "\n----download cni plugins at:"
-echo https://github.com/containernetworking/plugins/releases
+echo -e "\n----download cni plugins at:"
+echo https://github.com/containernetworking/plugins/releases/download/${CNI_VER}/cni-${CNI_VER}.tgz
 
-sleep 30
+sleep 20
+
+### prepare 'move docker-compose'
+echo -e "\nMoving 'docker-compose' to 'bin' dir..."
+if [ -f "docker-compose-Linux-x86_64" ]; then
+  mv -f docker-compose-Linux-x86_64 ../bin/docker-compose
+else
+  echo Please download 'docker-compose-Linux-x86_64' at 'https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE}/docker-compose-Linux-x86_64'
+fi
 
 ### prepare 'cfssl' cert tool suit
-echo "\nMoving 'cfssl' to 'bin' dir..."
+echo -e "\nMoving 'cfssl' to 'bin' dir..."
 if [ -f "cfssl_linux-amd64" ]; then
   mv -f cfssl_linux-amd64 ../bin/cfssl
 else
@@ -59,7 +65,7 @@ fi
 
 ### prepare 'etcd' binaries
 if [ -f "etcd-${ETCD_VER}-linux-amd64.tar.gz" ]; then
-  echo "\nextracting etcd binaries..."
+  echo -e "\nextracting etcd binaries..."
   tar zxf etcd-${ETCD_VER}-linux-amd64.tar.gz
   mv -f etcd-${ETCD_VER}-linux-amd64/etcd* ../bin
 else
@@ -68,7 +74,7 @@ fi
 
 ### prepare kubernetes binaries
 if [ -f "kubernetes-server-linux-amd64.tar.gz" ]; then
-  echo "\nextracting kubernetes binaries..."
+  echo -e "\nextracting kubernetes binaries..."
   tar zxf kubernetes-server-linux-amd64.tar.gz
   mv -f kubernetes/server/bin/kube-apiserver ../bin
   mv -f kubernetes/server/bin/kube-controller-manager ../bin
@@ -82,7 +88,7 @@ fi
 
 ### prepare docker binaries
 if [ -f "docker-${DOCKER_VER}.tgz" ]; then
-  echo "\nextracting docker binaries..."
+  echo -e "\nextracting docker binaries..."
   tar zxf docker-${DOCKER_VER}.tgz
   mv -f docker/docker* ../bin
   if [ -f "docker/completion/bash/docker" ]; then
@@ -94,7 +100,7 @@ fi
 
 ### prepare cni plugins, needed by flannel;
 if [ -f "cni-${CNI_VER}.tgz" ]; then
-  echo "\nextracting cni plugins binaries..."
+  echo -e "\nextracting cni plugins binaries..."
   tar zxf cni-${CNI_VER}.tgz
   mv -f bridge ../bin
   mv -f flannel ../bin
