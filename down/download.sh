dc26115f83dd71b99de5a6547c187facd9737e21
uxun <zhongshixun0201@gmail.com>
2019-03-11 12:38:55 +0800

    modify Multi-master Deployment version

--- down/download.sh
+++ down/download.sh
@@ -7,7 +7,7 @@ ETCD_VER=v3.2.24
 DOCKER_VER=18.06.3-ce
 CNI_VER=v0.6.0
 DOCKER_COMPOSE=1.23.0
-HARBOR=v1.6.0
+HARBOR=v1.6.3
 
 echo -e "\nNote1: Before this script, please finish downloading binaries manually from following urls."
 echo -e "\nNote2：If binaries are not ready, use "Ctrl + C" to stop this script."
